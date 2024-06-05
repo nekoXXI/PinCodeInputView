570fcaeb84edc5434ca885d5379416686c85bea6
Neko <nekoxxl@icloud.com>
2024-06-05 13:59:51 +0600

    fixed package names

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "PinCodeInputView",
+    name: "SahihInvestPinCodeInputView",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "PinCodeInputView",
+            name: "SahihInvestPinCodeInputView",
             targets: ["PinCodeInputView"]),
     ],
     targets: [

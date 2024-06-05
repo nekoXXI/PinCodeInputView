5da38dfaca0ad34981932a1296f8d687fda0aee5
Neko <nekoxxl@icloud.com>
2024-06-05 13:26:02 +0600

    fix package

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PinCodeInputView",
-            targets: ["PinCodeInputView"]),
+            targets: ["Sources/PinCodeInputView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PinCodeInputView"),
+            name: "Sources/PinCodeInputView"),
         .testTarget(
             name: "PinCodeInputViewTests",
             dependencies: ["PinCodeInputView"]),
